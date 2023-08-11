b2c379ec8ba0e064c420cca466d3a4d407aa7234
Albert Yin <albertapplea@gmail.com>
2023-08-10 22:41:06 -0500

    Get Method

--- collegegpt_be/importfiles/tests.py
+++ collegegpt_be/importfiles/tests.py
@@ -3,20 +3,20 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from importfiles.models import UploadedPDF
 
-class UploadedPDFListCreateViewTestCase(APITestCase):
-    def setUp(self):
-        self.url = reverse('uploadedpdf-list-create')
-        self.pdf_data = {'file_name': 'sample_syllabus.pdf', 'file': open('sample_syllabus.pdf', 'rb')}
-
-    def test_upload_pdf(self):
-        response = self.client.post(self.url, self.pdf_data, format='multipart')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(UploadedPDF.objects.count(), 1)
-        self.assertEqual(UploadedPDF.objects.get().file_name, 'sample_syllabus.pdf')
-
-    def test_get_uploaded_pdfs(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+#class UploadedPDFListCreateViewTestCase(APITestCase):
+#    def setUp(self):
+#        self.url = reverse('uploadedpdf-list-create')
+#        self.pdf_data = {'file_name': 'sample_syllabus.pdf', 'file': open('sample_syllabus.pdf', 'rb')}
+#
+#    def test_upload_pdf(self):
+#        response = self.client.post(self.url, self.pdf_data, format='multipart')
+#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#        self.assertEqual(UploadedPDF.objects.count(), 1)
+#        self.assertEqual(UploadedPDF.objects.get().file_name, 'sample_syllabus.pdf')
+#
+#    def test_get_uploaded_pdfs(self):
+#        response = self.client.get(self.url)
+#        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class UploadFileViewTestCase(APITestCase):
     def setUp(self):
@@ -26,3 +26,11 @@ class UploadFileViewTestCase(APITestCase):
     def test_upload_file(self):
         response = self.client.post(self.url, self.pdf_data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_ids(self):
+        #print out what the get function does and what it returns
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        ids = response.json()
+        for id in ids:
+            print(id)
--- collegegpt_be/importfiles/views.py
+++ collegegpt_be/importfiles/views.py
@@ -17,6 +17,18 @@ from importfiles.file_processing import extract_file_data
 
 credentials = boto3.Session(aws_access_key_id=config('AWS_OPENSEARCH_ACCESS_KEY'),aws_secret_access_key=config('AWS_OPENSEARCH_SECRET_ACCESS_KEY')).get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, 'us-east-2', 'es', session_token=credentials.token)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+vectorsearch = OpenSearchVectorSearch(
+            opensearch_url=config('OPENSEARCH_URL'),
+            index_name="test-index",
+            embedding_function=embeddings,
+            is_aoss=True,
+            http_auth=awsauth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection,
+        )
 # Create your views here.
 
 #This view function will handle the file upload
@@ -36,25 +48,24 @@ class UploadedPDFListCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class UploadFileView(APIView):
+    def get(self, request, format=None):
+        #search docs
+        response = vectorsearch.client.search(index='test-index', body={"query": {"match_all": {}}, "_source": ["text"]}, size=10000)
+        #extract IDs from response
+        ids = [hit['_source']['text'] for hit in response['hits']['hits']]
+        #extract source from metadata from response
+        #source = [hit['_source']['metadata']['text'] for hit in response['hits']['hits']]
+
+        return Response(ids, status=status.HTTP_200_OK)
+    
     def post(self, request, format=None):
         uploaded_file = request.FILES['file']
-        file_name = request.data['file_name']
-
-        extracted_text = extract_file_data(uploaded_file, file_name)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        extracted_text = extract_file_data(uploaded_file)
         split_documents = text_splitter.split_documents(extracted_text)
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-        
-        vectorsearch = OpenSearchVectorSearch.from_documents(
-            split_documents,
-            embeddings,
-            opensearch_url=config('OPENSEARCH_URL'),
-            http_auth=awsauth,
-            use_ssl=True,
-            verify_certs=True,
-            connection_class=RequestsHttpConnection,
-            index_name="test-index",
-        )
-        
+        vectorsearch.add_documents(split_documents)
         return Response(status=status.HTTP_201_CREATED)
+    
+    def delete(self, request, format=None):
+        vectorsearch.client.delete_by_query(index="test-index", body={"query": {"id": {request.data['id']}}})
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
